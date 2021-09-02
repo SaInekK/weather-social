6df2b84bea0abfc2a76570512f3db93528a239bd
SaInekK <mober228lol@gmail.com>
2021-09-02 17:09:48 +0300

    working app template added

--- project/forms.py
+++ project/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, DateField
-from wtforms.validators import DataRequired, Email, NumberRange
+from wtforms.validators import DataRequired, Length
 
 
 class PostForm(FlaskForm):
     date = DateField("Date: ", validators=[DataRequired()])
     name = StringField("Name: ", validators=[DataRequired()])
-    message = TextAreaField("Message: ", validators=[DataRequired()])
+    message = TextAreaField\
+        ("Message: ", validators=[DataRequired(),\
+            Length(min=5, max=400, message="Длина сообщения должна быть в промежутке 5<len<400")])
     submit = SubmitField("Submit")
 
 
